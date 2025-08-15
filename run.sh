92bd84570a23d31f2d713b6f8833346cf7fd02b7
Josen-B <baiqiaosen@gmail.com>
2025-08-14 19:05:29 -0600

    change framework

--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
-## 遍历 config/board 目录下的所有 toml 文件
+#!/bin/bash
+
 echo "start testing..."
 
-for file in config/board/*.toml; do
+## 遍历 config 下的所有目录
+for dir in config/*; do
     # 检查文件是否存在
-    if [ -f "$file" ]; then
-        # 1. 解析 board 配置文件
+    if [ -d "$dir" ]; then
+        # 1. 把 |平台接口配置项| 和 |vms的配置文件| 发送给 ostool
 
-        # 2. 把 |ostool的配置项| 和 |vms的配置文件| 发送给 ostool
+        # 2. 等待上电
 
-        # 3. 等待上电
+        # 3. 检查结果，检查是否超时
 
-        # 4. 检查结果，检查是否超时
-
-        echo "test passed: $file"
+        echo "test passed: $dir"
     else
-        echo "Error: file not found: $file"
+        echo "Error: dir not found: $dir"
         exit 1
     fi
 done
