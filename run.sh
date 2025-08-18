5fa3fb00b26d833f71711739d27aa50f53733eea
Josen-B <baiqiaosen@gmail.com>
2025-08-17 20:47:03 -0600

    modify send config file

--- run.sh
+++ run.sh
@@ -8,10 +8,10 @@ for dir in config/*; do
     # 检查文件是否存在
     if [ -d "$dir" ]; then
         echo "start test : $dir"
-        # 1. 把 |平台接口配置项| 和 |vm的配置文件| 发送给 ostool
-        # send .project.toml and vm_[plat]_[guestos].toml to ostool
+        # 1. 把 |内核启动配置文件| 和 |board配置文件| 发送给 ostool
+        # send .project.toml and board_[plat]_[guestos].toml to ostool
         cp "$dir/.project.toml" ../
-        # TODO: send vm_[plat]_[guestos].toml to ostool
+        # TODO: send board_[plat]_[guestos].toml to ostool
 
         # 2. 等待上电，执行测例
         cd ../ & ostool run uboot
@@ -26,9 +26,10 @@ for dir in config/*; do
         echo -n -e "$power_on_cmd" > $serial_port
 
         # 3. 检查结果，检查是否超时
-        # TODO
+        timeout=300  # 设置超时时间为300秒
 
         # 4. 重置，发送下电指令
+        # 发送对应门路的下电指令 A0(起始标识) 01(第一路) 00(下电) A1(校验码)
         check_code=$((0xA0 + serial_id))
         power_off_cmd=$(printf "A0%02X00%X" "$serial_id" "$check_code")
         echo "power off command: $power_off_cmd"
