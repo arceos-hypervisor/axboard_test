f95a0a1107607fe37b6293126f4d619b42f6a1a3
Josen-B <baiqiaosen@gmail.com>
2025-08-26 10:00:16 +0800

    Optimize the timeout mechanism

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
-echo "start testing..."
 serial_id=1
+device="/dev/ttyACM0"
+logfile="log.txt"
+max_ostool_time=120
+max_test_time=300
 
 send_config() {
     # send .project.toml and .board.toml to ostool
@@ -12,14 +14,13 @@ send_config() {
 start_ostool() {
     cd ../
     source ./activate.sh
-    ostool board-test | tee log.txt &
+    ostool board-test | tee $logfile &
     ostool_pid=$!
 }
 
 power_on() {
     # 发送对应门路的上电指令 A0(起始标识) 01(第一路) 01(上电) A2(校验码)
     serial_hex=$(printf "%02X" "$serial_id")
-    device="/dev/ttyACM$((serial_id-1))"
     stty -F "$device" 115200 cs8 -cstopb -parenb raw -echo -echoe -echok
 
     up_check_code=$(printf '%02X' $((0xA0 + $serial_hex + 0x01)))
@@ -40,6 +41,7 @@ power_off() {
 
 reset() {
     # 关闭ostool
+    echo "reset..."
     kill $ostool_pid
     deactivate
     cargo clean
@@ -62,12 +64,44 @@ check_result() {
     fi
 }
 
-timeout_handler() {
-    echo "Test timed out."
-    reset
-    exit 1
+ostool_timeout_check() {
+    elapsed=0
+    while (( elapsed < max_ostool_time )); do
+        sleep 1
+        if grep -qF ""等待 U-Boot 启动"" "$logfile"; then
+            echo "The keyword has been detected, Continue."
+            break
+        fi
+        ((elapsed++))
+    done
+
+    # 判断是否因超时跳出
+    if (( elapsed >= max_ostool_time )); then
+        echo "The keyword was not detected during the timeout and was redirected to reset."
+        reset
+        exit 1
+    fi
 }
 
+test_timeout_check() {
+    elapsed=0
+    while (( elapsed < max_test_time )); do
+        sleep 1
+        if grep -qF "Test passed" "$logfile"; then
+            echo "Test passed, Prepare to close."
+            break
+        fi
+        ((elapsed++))
+    done
+
+    # 判断是否因超时跳出
+    if (( elapsed >= max_test_time )); then
+        echo "The keyword was not detected during the timeout and was redirected to test_failed."
+        test_failed
+    fi
+}
+
+echo "start testing..."
 ## 遍历 config 下的所有目录
 for dir in config/*; do
     # 检查文件是否存在
@@ -78,9 +112,10 @@ for dir in config/*; do
 
         # 2. 启动ostool，上电运行测例
         start_ostool
-        sleep 30
+        ostool_timeout_check
+
         power_on
-        sleep 30
+        test_timeout_check
 
         # 3. 检查结果，检查是否超时
         check_result
